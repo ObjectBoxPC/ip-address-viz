96cbf3704de09146b0db7cf4e045c311b743f40f
Philip Chung <philipchung1995@yahoo.com>
2023-11-12 16:12:02 -0800

    Migrate to Preact
    D3 provides much more functionality than is needed here.
    Preact is much smaller and is sufficient for this application.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 'use strict';
 
-(function (d3) {
-	function identity(x) {
-		return x;
+(function (document, preact, preactHooks) {
+	var h = preact.h;
+
+	function range(max) {
+		var result = [];
+		for (var i = 0; i < max; i++) {
+			result.push(i);
+		}
+		return result;
 	}
 
 	function chunk(arr, chunkSize) {
-		return d3.range(Math.ceil(arr.length / chunkSize))
+		return range(Math.ceil(arr.length / chunkSize))
 			.map(function (i) { return arr.slice(i * chunkSize, (i + 1) * chunkSize); });
 	}
 
@@ -21,93 +27,157 @@
 		return 'x' + i.toString(16) + '00::/8';
 	}
 
-	function buildTableHeader(table) {
-		var headerRow = table.append('thead').append('tr');
-		headerRow.append('td');
-		headerRow
-			.selectAll('th')
-			.data(d3.range(gridColumns).map(slashEightColumnHeader))
-			.join('th')
-			.attr('scope', 'col')
-			.text(identity);
-	}
+	function DataTable() {
+		var dataState = preactHooks.useState([]);
 
-	function buildTableBody(table, data) {
-		table
-			.append('tbody')
-			.selectAll('tr')
-			.data(data)
-			.join('tr')
-			.each(buildDataRow);
-	}
-
-	function buildDataRow(data, i) {
-		var row = d3.select(this);
-		row
-			.append('th')
-			.attr('scope', 'row')
-			.datum(slashFourRowHeader(i))
-			.text(identity);
-		if (data.type === 'various') {
-			buildComplexRow(row, data);
-		} else {
-			buildSimpleRow(row, data);
-		}
-	}
-
-	function buildComplexRow(row, data) {
-		row
-			.selectAll('td')
-			.data(data.subblocks)
-			.join('td')
-			.each(function (d) {
-				var cell = d3.select(this);
-				if (d.type === 'various') {
-					buildComplexCell(cell, d);
-				} else {
-					buildSimpleCell(cell, d);
-				}
+		preactHooks.useEffect(function () {
+			var xhr = new XMLHttpRequest();
+			xhr.open('GET', 'blocks.json');
+			xhr.addEventListener('load', function () {
+				var data = JSON.parse(xhr.responseText);
+				dataState[1](data);
 			});
+			xhr.send();
+		}, []);
+
+		return h(
+			'table',
+			null,
+			h(
+				DataTableHeader,
+				null
+			),
+			h(
+				DataTableBody,
+				{ data: dataState[0] }
+			)
+		);
 	}
 
-	function buildComplexCell(cell, data) {
-		cell.classed('block-fine', true);
-		cell
-			.append('table')
-			.selectAll('tr')
-			.data(chunk(data.subblocks, fineGridColumns))
-			.join('tr')
-			.selectAll('td')
-			.data(identity)
-			.join('td')
-			.attr('class', function (d) { return 'block-' + d.type; });
-		cell
-			.append('div')
-			.classed('block-fine-overlay', true)
-			.text(data.description)
-			.attr('title', data.prefix);
+	function DataTableHeader() {
+		return h(
+			'thead',
+			null,
+			h(
+				'tr',
+				null,
+				h(
+					'td',
+					null
+				),
+				range(gridColumns).map(function (i) {
+					return h(
+						'th',
+						{ scope: 'col', key: i },
+						slashEightColumnHeader(i)
+					);
+				})
+			)
+		);
 	}
 
-	function buildSimpleCell(cell, data) {
-		cell
-			.classed('block-small block-' + data.type, true)
-			.text(data.description)
-			.attr('title', data.prefix);
+	function DataTableBody(props) {
+		return h(
+			'tbody',
+			null,
+			props.data.map(function (block, i) {
+				return h(
+					DataRow,
+					{ data: block, index: i, key: block.prefix }
+				)
+			})
+		);
 	}
 
-	function buildSimpleRow(row, data) {
-		row
-			.append('td')
-			.attr('colspan', gridColumns)
-			.classed('block-large block-' + data.type, true)
-			.text(data.description)
-			.attr('title', data.prefix);
+	function DataRow(props) {
+		return h(
+			'tr',
+			null,
+			h(
+				'th',
+				{ scope: 'row' },
+				slashFourRowHeader(props.index)
+			),
+			props.data.type === 'various'
+				? h(
+					ComplexDataRowContents,
+					{ data: props.data }
+				)
+				: h(
+					SimpleDataRowContents,
+					{ data: props.data }
+				)
+		);
 	}
 
-	var table = d3.select('main').append('table');
-	buildTableHeader(table);
-	d3.json('blocks.json')
-		.then(function (data) {
-			buildTableBody(table, data);
+	function ComplexDataRowContents(props) {
+		return props.data.subblocks.map(function (block) {
+			return block.type === 'various'
+				? h(
+					ComplexDataCell,
+					{ data: block, key: block.prefix }
+				)
+				: h(
+					SimpleDataCell,
+					{ data: block, key: block.prefix }
+				);
 		});
-})(d3);
+	}
+
+	function ComplexDataCell(props) {
+		return h(
+			'td',
+			{ 'class': 'block-fine' },
+			h(
+				'table',
+				null,
+				chunk(props.data.subblocks, fineGridColumns)
+					.map(function (blockChunk) {
+						return h(
+							'tr',
+							null,
+							blockChunk.map(function (block) {
+								return h(
+									'td',
+									{ 'class': 'block-' + block.type }
+								);
+							})
+						);
+					})
+			),
+			h(
+				'div',
+				{
+					title: props.data.prefix,
+					'class': 'block-fine-overlay',
+				},
+				props.data.description
+			)
+		);
+	}
+
+	function SimpleDataCell(props) {
+		return h(
+			'td',
+			{
+				title: props.data.prefix,
+				'class': 'block-small block-' + props.data.type
+			},
+			props.data.description
+		);
+	}
+
+	function SimpleDataRowContents(props) {
+		return h(
+			'td',
+			{
+				colspan: gridColumns,
+				title: props.data.prefix,
+				'class': 'block-large block-' + props.data.type,
+			},
+			props.data.description
+		);
+	}
+
+	preact.render(h(DataTable), document.getElementsByTagName('main')[0]);
+})(document, preact, preactHooks);
